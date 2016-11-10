fc41664c88e210f0473e01a936f0e944f3441a71
bazebimio <bazebimio@J205-15.infogestion.local>
2016-11-10 10:57:47 +0100

    Change le formulaire en fonction du type d'utilisateur + ajout lien

--- vues/v_connexion.php
+++ vues/v_connexion.php
@@ -1,21 +1,80 @@
 ﻿<div id="contenu">
-      <h2>Identification utilisateur</h2>
+
+	<?php
+	// test , si il s'agit d'un comptable ou un visiteur , et change le résultat du formulaire en fonction de l'utilisateur
+	if (! isset ( $_REQUEST ['utilisateur'] )) {
+		$utilisateur = "visiteur";
+	} else {
+		$utilisateur = $_REQUEST ['utilisateur'];
+	}
+	if ($utilisateur == "visiteur") {
+		?>
+		<h2>Identification utilisateur</h2>
 
 
-<form method="POST" action="index.php?uc=connexion&action=valideConnexion" id="FormConnexion">
-   
-    
-			<p>
-       <label for="nom">Login*</label>
-       <input id="login" type="text" name="login"  size="30" maxlength="45">
-      </p>
-			<p>
-				<label for="mdp">Mot de passe*</label>
-			  <input id="mdp"  type="password"  name="mdp" size="30" maxlength="45">
-      </p>
-         <input type="submit" value="Valider" name="valider">
-         <input type="reset" value="Annuler" name="annuler"> 
-      </p>
-</form>
+	<form method="POST"
+		action="index.php?uc=connexion&action=valideConnexion"
+		id="FormConnexion">
+
+
+		<p>
+			<label for="nom">Login*</label> <input id="login" type="text"
+				name="login" size="30" maxlength="45">
+		</p>
+		<p>
+			<label for="mdp">Mot de passe*</label> <input id="mdp"
+				type="password" name="mdp" size="30" maxlength="45">
+		</p>
+		<input name="utilisateur" type="hidden" value="visiteur"> <input
+			type="submit" value="Valider" name="valider"> <input type="reset"
+			value="Annuler" name="annuler">
+		</p>
+	</form>
+	<p>
+		<a href="index.php?uc=connexion&utilisateur=comptable"> Se connecter
+			en tant que comptable </a>
+	</p>
+	<?php
+	} elseif ($utilisateur == "comptable") {
+		?>
+		<h2>Identification utilisateur</h2>
+	<form method="POST"
+		action="index.php?uc=connexion&action=valideConnexion&utilisateur=comptable"
+		id="FormConnexion">
+
+
+		<p>
+			<label for="nom">Login*</label> <input id="login" type="text"
+				name="login" size="30" maxlength="45">
+		</p>
+		<p>
+			<label for="mdp">Mot de passe*</label> <input id="mdp"
+				type="password" name="mdp" size="30" maxlength="45">
+		</p>
+		<input name="utilisateur" type="hidden" value="comptable" /> <input
+			type="submit" value="Valider" name="valider" /> <input type="reset"
+			value="Annuler" name="annuler">
+		</p>
+	</form>
+	<p>
+		<a href="index.php?uc=connexion&utilisateur=visiteur"> Se connecter en
+			tant que visiteur </a>
+	<?php } ?> 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 </div>
