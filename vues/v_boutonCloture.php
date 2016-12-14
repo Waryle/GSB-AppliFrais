50d25ffe38f4286cedc7832b9d96f34eb9f73f01
ducrocq <ducrocq@J205-12.infogestion.local>
2016-12-14 14:24:55 +0100

    Correction condition affichage du bouton de clôture

--- vues/v_boutonCloture.php
+++ vues/v_boutonCloture.php
@@ -1,12 +1,12 @@
 <?php
 	$jourCourant = date("d") ;
 	if(date("m") == "1")
-		$moisPrecedent = date("Y")."12" ;
+		$moisPrecedent = date("Y")-1 . "12" ;
 	else
 		$moisPrecedent = date("Y").date("m")-1;
-	$fiche = $pdo->getEtat($_SESSION['idVisiteur'], $mois) ;
+	$fiche = $pdo->getEtat($_SESSION['idVisiteur'], $moisPrecedent) ;
 	
-	if($jourCourant >= 1 && $jourCourant <= 10 && $fiche[0] == 'CR') {
+	if($jourCourant >= 1 && $jourCourant <= 14 && $fiche[0] == 'CR') {
 ?>
 	<p>
 		<form action=index.php?uc=gererFrais&action=cloturerFicheFrais method="POST">
